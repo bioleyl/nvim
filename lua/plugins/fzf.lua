b5c08ca89ec20d6b73a38ece22b3befdd1577c40
Leal Bioley <lb@syware.ch>
2025-04-14 19:09:35 +0200

    Add search symbols keymaps

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,50 +1,87 @@
 return {
-  "ibhagwan/fzf-lua",
+  'ibhagwan/fzf-lua',
   -- optional for icon support
   -- dependencies = { "nvim-tree/nvim-web-devicons" },
   -- or if using mini.icons/mini.nvim
-  dependencies = { "echasnovski/mini.icons" },
+  dependencies = { 'echasnovski/mini.icons' },
   opts = {},
   keys = {
-    { "<leader>ff",
-      function() require("fzf-lua").files() end,
-      desc = "[F]ind [F]ile",
+    {
+      '<leader>ff',
+      function()
+        require('fzf-lua').files()
+      end,
+      desc = '[F]ind [F]ile',
     },
-    { "<leader>fg",
-      function() require("fzf-lua").live_grep() end,
-      desc = "[F]ind by [G]rep",
+    {
+      '<leader>fg',
+      function()
+        require('fzf-lua').live_grep()
+      end,
+      desc = '[F]ind by [G]rep',
     },
-    { "<leader>fc",
-      function() require("fzf-lua").files({cwd=vim.fn.stdpath("config")}) end,
-      desc = "[F]ind in neovim [C]onfig",
+    {
+      '<leader>fc',
+      function()
+        require('fzf-lua').files { cwd = vim.fn.stdpath 'config' }
+      end,
+      desc = '[F]ind in neovim [C]onfig',
     },
-    { "<leader>fb",
-      function() require("fzf-lua").builtin() end,
-      desc = "[F]ind in [B]uiltin FZF",
+    {
+      '<leader>fs',
+      function()
+        require('fzf-lua').lsp_document_symbols()
+      end,
+      desc = '[F]ind in [S]ymbols',
     },
-    { "<leader>fk",
-      function() require("fzf-lua").keymaps() end,
-      desc = "[F]ind in [K]eymaps",
+    {
+      '<leader>fb',
+      function()
+        require('fzf-lua').builtin()
+      end,
+      desc = '[F]ind in [B]uiltin FZF',
     },
-    { "<leader>fr",
-      function() require("fzf-lua").resume() end,
-      desc = "[F]ind [R]esume",
+    {
+      '<leader>fk',
+      function()
+        require('fzf-lua').keymaps()
+      end,
+      desc = '[F]ind in [K]eymaps',
     },
-    { "<leader><leader>",
-      function() require("fzf-lua").buffers() end,
-      desc = "[ ] Find existing buffers",
+    {
+      '<leader>fr',
+      function()
+        require('fzf-lua').resume()
+      end,
+      desc = '[F]ind [R]esume',
     },
-    { "<leader>/",
-      function() require("fzf-lua").lgrep_curbuf() end,
-      desc = "[/] Livre grep the current buffer",
+    {
+      '<leader><leader>',
+      function()
+        require('fzf-lua').buffers()
+      end,
+      desc = '[ ] Find existing buffers',
     },
-    { "<leader>fw",
-      function() require("fzf-lua").grep_cword() end,
-      desc = "[F]ind current [W]ord",
+    {
+      '<leader>/',
+      function()
+        require('fzf-lua').lgrep_curbuf()
+      end,
+      desc = '[/] Livre grep the current buffer',
     },
-    { "<leader>fW",
-      function() require("fzf-lua").grep_cWORD() end,
-      desc = "[F]ind current [W]ORD",
-    }
-  }
+    {
+      '<leader>fw',
+      function()
+        require('fzf-lua').grep_cword()
+      end,
+      desc = '[F]ind current [W]ord',
+    },
+    {
+      '<leader>fW',
+      function()
+        require('fzf-lua').grep_cWORD()
+      end,
+      desc = '[F]ind current [W]ORD',
+    },
+  },
 }
